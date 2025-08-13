4b82c79edf01bfe3675c4dd0063d8a8da65f604b
NeutrinoZh <neutrinozh@gmail.com>
2025-08-13 21:59:03 +0300

    seo

--- apps/landing/app/layout.tsx
+++ apps/landing/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.scss";
-import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Lumenwake Studio",
   description: "Game development studio",
+  keywords: ["gamedev", "development", "games", "studio", "lumen", "lumenwake", "company", "spacebac"],
+  openGraph: {
+    images: "https://lumenwake.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fspacebac-preview.c8beb6de.png&w=1920&q=75"
+  }
 };
 
 export default function RootLayout({
@@ -14,14 +17,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <meta property="og:title" content="Lumenwake Studio" />
-        <meta property="og:description" content="Game Development Studio" />
-        <meta property="og:image" content="https://lumenwake.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fspacebac-preview.c8beb6de.png&w=1920&q=75" />
-        <meta name="twitter:card" content="lumenwake" />
-        <meta property="og:type" content="website"></meta>
-        <meta name="keywords" content="gamedev, development, games, studio, lumen, lumenwake, company, spacebac"></meta>
-      </Head>
       <body>
         {children}
       </body>
