76d1b6dc69d7e61f4443c4ba9ccfb9c413473803
NeutrinoZh <neutrinozh@gmail.com>
2025-08-13 22:01:42 +0300

    seo

--- apps/landing/app/layout.tsx
+++ apps/landing/app/layout.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
   description: "Game development studio",
   keywords: ["gamedev", "development", "games", "studio", "lumen", "lumenwake", "company", "spacebac"],
   openGraph: {
-    images: "https://lumenwake.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fspacebac-preview.c8beb6de.png&w=1920&q=75"
+    images: "https://lumenwake.com/spacebac-preview.png"
   }
 };
 
