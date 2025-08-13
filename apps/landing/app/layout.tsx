b5318308926aa4fa07aec12a59677f3e83319554
NeutrinoZh <neutrinozh@gmail.com>
2025-08-13 21:50:18 +0300

    seo

--- apps/landing/app/layout.tsx
+++ apps/landing/app/layout.tsx
@@ -17,8 +17,10 @@ export default function RootLayout({
       <Head>
         <meta property="og:title" content="Lumenwake Studio" />
         <meta property="og:description" content="Game Development Studio" />
-        <meta property="og:image" content="https://lumenwake.com/spacebac-preview.png" />
+        <meta property="og:image" content="https://lumenwake.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fspacebac-preview.c8beb6de.png&w=1920&q=75" />
         <meta name="twitter:card" content="lumenwake" />
+        <meta property="og:type" content="website"></meta>
+        <meta name="keywords" content="gamedev, development, games, studio, lumen, lumenwake, company, spacebac"></meta>
       </Head>
       <body>
         {children}
